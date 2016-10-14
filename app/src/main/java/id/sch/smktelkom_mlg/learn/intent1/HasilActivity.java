57e094f76258562d600b1bfe1e07f600279ad1e4
Avilia <liaaviliaistiqomah@gmail.com>
2016-10-14 10:10:04 +0700

    startActivity Part 2

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -3,6 +3,8 @@ package id.sch.smktelkom_mlg.learn.intent1;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.TextView;
+
 
 public class HasilActivity extends AppCompatActivity {
 
@@ -11,8 +13,17 @@ public class HasilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil);
 
+        String nama = getIntent().getStringExtra(MainActivity.NAMA);
+        int umur = getIntent().getIntExtra(MainActivity.UMUR, 0);
+        int yearNow = 2016;
+        int tahunLahir = yearNow - umur;
+
+        TextView tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHasil.setText(nama + " lahir pada tahhun " + tahunLahir);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle(" Hasil");
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
